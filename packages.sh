75b3de940859eb146343b0dbb26b551308129c90
Benjamin Cook <bcook254@gmail.com>
2025-03-07 11:34:27 -0700

    Allow multiple packages manifests in packages.sh

--- packages.sh
+++ packages.sh
@@ -2,17 +2,17 @@
 
 set -oeux pipefail
 
-PACKAGE_MANIFEST=$1
+PACKAGE_MANIFESTS="$@"
 RELEASE="$(rpm -E %fedora)"
 
 # build list of all packages requested for inclusion
-INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+INCLUDED_PACKAGES=($(sort -du <<<"$(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" $PACKAGE_MANIFEST))
+                             | sort | unique[]" $PACKAGE_MANIFESTS)" | tr '\n' ' '))
 # build list of all packages requested for exclusion
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+EXCLUDED_PACKAGES=($(sort -du <<<"$(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" $PACKAGE_MANIFEST))
+                             | sort | unique[]" $PACKAGE_MANIFESTS)" | tr '\n' ' '))
 
 # ensure exclusion list only contains packages already present on image
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
@@ -40,9 +40,9 @@ fi
 
 # check if any excluded packages are still present
 # (this can happen if an included package pulls in a dependency)
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+EXCLUDED_PACKAGES=($(sort -du <<<"$(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" $PACKAGE_MANIFEST))
+                             | sort | unique[]" $PACKAGE_MANIFESTS)" | tr '\n' ' '))
 
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
