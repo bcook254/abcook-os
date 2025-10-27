0d349e5df22e66a790c1f978fca3bec1aeeebfde
Benjamin Cook <bcook254@gmail.com>
2025-10-27 10:18:03 -0600

    Remove unused RPM and Flatpak repositories

--- install.sh
+++ install.sh
@@ -65,6 +65,17 @@ if [[ "$IMAGE_NAME" == "silverblue" ]]; then
     dnf5 versionlock add gnome-software
 fi
 
+# Remove Fedora Flatpak and related packages
+dnf5 remove -y \
+    fedora-flathub-remote \
+    fedora-third-party
+
+# Add Flathub to the image for eventual application
+mkdir -p /etc/flatpak/remotes.d/
+curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
+# Fedora Flatpak service is a part of the flatpak package, ensure it's overridden by moving to replace it at the end of the build.
+mv -f /usr/lib/systemd/system/flatpak-add-flathub-repos.service /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+
 # Install packages
 /ctx/packages.sh /ctx/packages.json /ctx/packages.${ARCH}.json
 
