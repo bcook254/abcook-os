e6173ac8baf6b08025a2cf06c2f65608db29d151
Benjamin Cook <bcook254@gmail.com>
2026-03-25 10:10:05 -0600

    Enable F44, remove all surface builds

--- surface/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -oeux pipefail
-
-if grep -q "silverblue" <<< "${IMAGE_NAME}"; then
-    systemctl enable dconf-update
-fi
-systemctl enable fprintd
-systemctl enable surface-hardware-setup
